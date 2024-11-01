8cedff158f41d2d8240dacc146a51feb66a79e37
Valeriy95 <vmariukhnyk@gmail.com>
2024-11-01 22:26:43 +0300

    feat: add four layout sections

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json"
+      },
+      globals: globals.browser
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettierPlugin
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettierConfig.rules,
+      "prettier/prettier": "error"
+    }
+  },
+  pluginJs.configs.recommended
+];
